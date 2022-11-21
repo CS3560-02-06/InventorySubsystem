12654362f726328018671179fd3226313894b3ef
OrbinWaffle <79740022+OrbinWaffle@users.noreply.github.com>
2022-11-20 21:04:31 -0800

    added getters/setters, fixed some data types

--- InventoryItem.java
+++ InventoryItem.java
@@ -10,5 +10,41 @@ public class InventoryItem
    String color;
    Date receiptDate;
    Date expirationDate;
-   String location;
+   int locationID;
+
+   // Constructor
+   public InventoryItem(int newPID, int newIID, double newPrice, int newAmount, double newSize, String newColor, Date newRDate, Date newEDate, int newLoc)
+   {
+      productID = newPID;
+      inventoryID = newIID;
+      price = newPID;
+      amountInStock = newAmount;
+      size = newSize;
+      color = newColor;
+      receiptDate = newRDate;
+      expirationDate = newEDate;
+      locationID = newLoc;
+   }
+
+   // Getters
+   public int getProductID(){return productID;}
+   public int getInventoryID(){return inventoryID;}
+   public double getPrice(){return price;}
+   public int getAmount(){return amountInStock;}
+   public double getSize(){return size;}
+   public String getColor(){return color;}
+   public Date getRecDate(){return receiptDate;}
+   public Date getExpDate(){return expirationDate;}
+   public int getLocation(){return locationID;}
+
+   // Setters
+   public void setProductID(int newID){productID = newID;}
+   public void setInventoryID(int newID){inventoryID = newID;}
+   public void setPrice(int newPrice){price = newPrice;}
+   public void setAmount(int newAmount){amountInStock = newAmount;}
+   public void setSize(int newSize){size = newSize;}
+   public void setColor(String newColor){color = newColor;}
+   public void setRecDate(Date newDate){receiptDate = newDate;}
+   public void setExpDate(Date newDate){expirationDate = newDate;}
+   public void setLocation(int newLocID){locationID = newLocID;}
 }
--- ProductItem.java
+++ ProductItem.java
@@ -2,7 +2,31 @@ public class ProductItem
 {
    int productID;
    String name;
-   String category;
-   String brand;
-   String decription;
+   int categoryID;
+   int brandID;
+   String description;
+
+   // Constructor
+   public ProductItem(int newID, String newName, int newCategory, int newBrand, String newDesc)
+   {
+      productID = newID;
+      name = newName;
+      categoryID = newCategory;
+      brandID = newBrand;
+      description = newDesc;
+   }
+
+   // Getters
+   public int getProductID(){return productID;}
+   public String getName(){return name;}
+   public int getCategory(){return categoryID;}
+   public int getBrand(){return brandID;}
+   public String getDesc(){return description;}
+
+   // Setters
+   public void setProductID(int newID){productID = newID;}
+   public void setName(String newName){name = newName;}
+   public void setCategory(int newCategory){categoryID = newCategory;}
+   public void setBrand(int newBrand){brandID = newBrand;}
+   public void setDesc(String newDesc){description = newDesc;}
 }
