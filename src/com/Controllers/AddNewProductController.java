22cfcb43fa3c6c0294bddc7c7a65bb1a91b2f76e
Kayla <85607915+kltang6328@users.noreply.github.com>
2022-11-20 22:24:52 -0800

    Create AddNewProductController.java

---
+++ src/com/Controllers/AddNewProductController.java
@@ -0,0 +1,38 @@
+package com.Controllers;
+
+import java.io.IOException;
+
+import com.InventoryManager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
+public class AddProductController {
+
+    @FXML
+    private MenuItem addProduct;
+    @FXML
+    private MenuItem searchProduct;
+    @FXML
+    private MenuItem searchInventory;
+    
+    public AddProductController() {
+
+    }
+
+    public void SelectAddProduct(ActionEvent event) throws IOException {
+        InventoryManager m = new InventoryManager();
+        m.ChangeScene("com/addnewproduct.fxml");
+    }
+
+    public void SelectSearchProduct(ActionEvent event) throws IOException {
+        InventoryManager m = new InventoryManager();
+        m.ChangeScene("com/SearchProduct.fxml");
+    }
+
+    public void SelectSearchInventory(ActionEvent event) throws IOException {
+        InventoryManager m = new InventoryManager();
+        m.ChangeScene("com/SearchInventory.fxml");
+    }
+}
